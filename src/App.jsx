004559a9a48ba9f4f4dd6d232fdad65ed3f1d189
Celina Garcia <cdummynet@gmail.com>
2023-06-05 23:37:44 -0700

    change db link

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 		<>
 			<h1>511 Group 3</h1>
 			<TableauEmbed
-				sourceUrl="https://public.tableau.com/views/Spotify_511_edm/Dashboard_Demo"
+				sourceUrl="https://public.tableau.com/views/Spotify_EDM_Final/Dashboard_Demo"
 				width={"100vw"}
 				height={"100vh"}
 			/>
